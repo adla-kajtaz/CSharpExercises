0d9b7cc29601b371e3d2c89967844245c9b3c6bc
Adla Kajtaz <adla.kajtaz@edu.fit.ba>
2024-12-15 18:03:06 +0100

    added exercise 2

---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int number = x;
+        int palindrome = 0;
+        while (x > 0)
+        {
+            palindrome = palindrome * 10 + x % 10;
+            x /= 10;
+        }
+        if (palindrome == number)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Solution solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(121));
+    }
+}
