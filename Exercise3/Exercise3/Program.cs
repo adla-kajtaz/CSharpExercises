fe2878e3e1c6ec7c6b2737a5a7dbd8a023380e3b
Adla Kajtaz <adla.kajtaz@edu.fit.ba>
2024-12-16 17:45:38 +0100

    added exercise 3

---
+++ Exercise3/Exercise3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Solution
+{
+    public  int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanVaules = new Dictionary<char, int>() {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int total = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int current = romanVaules[s[i]];
+            if (i + 1 < s.Length && current < romanVaules[s[i + 1]])
+            {
+                total -= current;
+            }
+            else
+            {
+                total += current;
+            }
+        }
+        return total;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Solution solution = new Solution();
+        Console.WriteLine("III is " + solution.RomanToInt("III"));
+        Console.WriteLine("LVIII is " + solution.RomanToInt("LVIII"));
+        Console.WriteLine("MCMXCIV is " + solution.RomanToInt("MCMXCIV"));
+    }
+}
